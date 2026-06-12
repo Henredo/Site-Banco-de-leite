b2000c55574ffa70aa0e49957cdc91354ae1cf7d
Henry M. Leme <henrymleme1000@gmail.com>
2026-06-12 19:23:49 -0300

    Delete base.js

--- src/script/base.js
+++
@@ -1,4 +0,0 @@
-document.write('<iframe id="barra-base" src="https://barra.to.gov.br/base.php?saude&ati=1" frameborder="no" scrolling="no" width="100%" height="450px"></iframe>');
-
-var iframeBarraBase = document.getElementById('barra-base');
-function postMessageBarraBase(data) { iframeBarraBase.contentWindow.postMessage(data,'https://www.to.gov.br/'); }
